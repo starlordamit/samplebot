c8aa652372acacff0c6bdb814cd730b8b69a32fc
starlordamit <59335027+starlordamit@users.noreply.github.com>
2021-07-05 15:20:09 +0530

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,28 @@
+from telegram import *
+from telegram.ext import *
+
+bot = Bot("1804803062:AAEtGz4iN-_7VXqHSgR6jwzE_dHk4qzrSuo")
+#print(bot.get_me())
+updater = Updater("1804803062:AAEtGz4iN-_7VXqHSgR6jwzE_dHk4qzrSuo",use_context=True)
+
+def testfunction(update:Update,context:CallbackContext):
+    bot.send_message(
+        chat_id=update.effective_chat.id,
+        text="tutorial link :LOL",
+        )
+        
+dispatcher = updater.dispatcher
+start_value = CommandHandler('motion',testfunction)
+dispatcher.add_handler(start_value)
+
+def testfunction2(update:Update,context:CallbackContext):
+    bot.send_message(
+        chat_id=update.effective_chat.id,
+        text="Hellow g",
+        )
+        
+dispatcher = updater.dispatcher
+start_value1 = CommandHandler('motion2',testfunction2)
+dispatcher.add_handler(start_value1)
+
+updater.start_polling()
